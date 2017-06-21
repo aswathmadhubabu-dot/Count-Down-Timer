c5ea7eed55bf1896b3f03807bf1005d885624508
ases573 <ases573@gmail.com>
2017-06-21 14:51:34 +0530

    "[GIT AUTO COMMIT]: Wed Jun 21 2017 14:51:34 GMT+0530 (India Standard Time)"

--- src/clock.jsx
+++ src/clock.jsx
@@ -3,27 +3,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Clock extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
-            days : 0,
-            hours :0,
-            minutes :0,
-            seconds :0
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
         }
         console.log(this.props.deadline);
+
+    }
+    getTimeUntil(deadline) {
         var d = new Date();
-        const result = Date.parse(this.props.deadline)-Date.parse(d.getDate());
+        const result = Date.parse(deadline) - Date.parse(d.getDate());
         console.log(result);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="col-md-12 container-fluid text-center">
-            <div className="col-md-3 text-fluid" id="clockDays">{this.state.days}Days</div>
-            <div className="col-md-3 text-fluid" id="clockHours">{this.state.hours}Hours</div>
-            <div className="col-md-3 text-fluid" id="clockMinutes">{this.state.minutes}Minutes</div>
-            <div className="col-md-3 text-fluid" id="clockSeconds">{this.state.seconds}Seconds</div>
+                <div className="col-md-3 text-fluid" id="clockDays">{this.state.days}Days</div>
+                <div className="col-md-3 text-fluid" id="clockHours">{this.state.hours}Hours</div>
+                <div className="col-md-3 text-fluid" id="clockMinutes">{this.state.minutes}Minutes</div>
+                <div className="col-md-3 text-fluid" id="clockSeconds">{this.state.seconds}Seconds</div>
             </div>
         )
     }
