c295895899d636404f06988058c2e58e8692f936
ases573 <ases573@gmail.com>
2017-06-21 14:25:34 +0530

    "[GIT AUTO COMMIT]: Wed Jun 21 2017 14:25:34 GMT+0530 (India Standard Time)"

--- src/clock.jsx
+++ src/clock.jsx
@@ -14,7 +14,7 @@ class Clock extends Component {
     }
     render(){
         return(
-            <div className="col-md-12 container-fluid text-center text-fluid">
+            <div className="col-md-12 container-fluid text-center">
             <div className="col-md-3 text-fluid" id="clockDays">{this.state.days}Days</div>
             <div className="col-md-3 text-fluid" id="clockHours">{this.state.hours}Hours</div>
             <div className="col-md-3 text-fluid" id="clockMinutes">{this.state.minutes}Minutes</div>
