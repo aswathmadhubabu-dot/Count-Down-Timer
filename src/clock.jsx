3ec05ab4aa7fc4444504775a1f9ad342fff272e0
ases573 <ases573@gmail.com>
2017-06-21 14:17:34 +0530

    "[GIT AUTO COMMIT]: Wed Jun 21 2017 14:17:34 GMT+0530 (India Standard Time)"

---
+++ src/clock.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Clock extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            days : 0,
+            hours :0,
+            minutes :0,
+            seconds :0
+        }
+    }
+    render(){
+        return{
+
+        }
+    }
+}
